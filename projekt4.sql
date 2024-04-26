9aaa896a52e8af2c0136e24b693fa7fcf13dcfdb
Lukas Katona <katona.lukasek@gmail.com>
2024-04-27 00:01:05 +0200

    feat: more stuff for proj4

--- projekt4.sql
+++ projekt4.sql
@@ -134,13 +134,33 @@ CREATE SEQUENCE odber_zamestnanec_seq;
 -- Vytvoreni triggru
 
 -- trigger na nastaveni stavu objednavky a data pri vytvoreni nove objednavky
-DROP TRIGGER Objednavka_vytvorena_trigger;
-CREATE TRIGGER Objednavka_vytvorena_trigger
+CREATE OR REPLACE TRIGGER Objednavka_vytvorena_trigger
 BEFORE INSERT ON Objednavka
 FOR EACH ROW
 BEGIN
+    IF :NEW.fk_id_zarizeni_objednavatel = :NEW.fk_id_zarizeni_dodavatel THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Zarizeni objednavatel a dodavatel nesmi byt stejne');
+    END IF;
+
     :NEW.stav := 'vytvorena';
     :NEW.datum_vytvoreni := SYSDATE;
+END;
+/
+
+-- trigger na kontrolu typu krve pri vytvoreni noveho odberu
+CREATE OR REPLACE TRIGGER Odber_typ_krve_trigger
+BEFORE INSERT ON Odber
+FOR EACH ROW
+DECLARE
+    typ Darce.typ_krve%TYPE;
+BEGIN
+    SELECT typ_krve INTO typ
+    FROM Darce
+    WHERE id_darce_fk_osoba = :NEW.fk_id_darce;
+
+    IF :NEW.typ_krve != typ THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Typ krve neodpovida typu krve darce');
+    END IF;
 END;
 /
 
@@ -167,14 +187,14 @@ CREATE OR REPLACE PROCEDURE Dokonceni_objednavky(
 END;
 /
 
--- procedura pro pridani odberu do objednavky
+-- procedura pro pridani odberu do objednavky (S KURZOREM)
 -- prida odbery nachazejici se v zarizeni dodavatele objednavky, dokud neni dosazene mnozstvi krve polozky objednavky
 CREATE OR REPLACE PROCEDURE Pridani_odberu_do_objednavky(
     id_polozka IN Polozka_objednavky.id_polozka%TYPE
 ) AS
     id_objednavka Polozka_objednavky.fk_id_objednavka%TYPE;
     mnozstvi_krve_polozka INTEGER;
-    id_odber Odber.id_odber%TYPE;
+    id_odber_cursor Odber.id_odber%TYPE;
     mnozstvi_krve_odber INTEGER;
     mnozstvi_krve_odbery_celkem INTEGER;
     -- cursor pro vyber odberu, ktere se maji pridat do objednavky
@@ -196,7 +216,6 @@ CREATE OR REPLACE PROCEDURE Pridani_odberu_do_objednavky(
             FROM Polozka_objednavky
             WHERE id_polozka = Pridani_odberu_do_objednavky.id_polozka
         );
-
 BEGIN
     SELECT fk_id_objednavka INTO id_objednavka
         FROM Polozka_objednavky
@@ -211,12 +230,64 @@ BEGIN
     LOOP
         -- ukonci pokud je kurzor prazdny nebo pokud je celkove mnozstvi krve odberu vetsi nez mnozstvi krve polozky objednavky
         EXIT WHEN odbery_cursor%NOTFOUND OR mnozstvi_krve_odbery_celkem >= mnozstvi_krve_polozka;
-        FETCH odbery_cursor INTO id_odber, mnozstvi_krve_odber;
+        FETCH odbery_cursor INTO id_odber_cursor, mnozstvi_krve_odber;
         UPDATE Odber SET fk_id_objednavka = id_objednavka
-        WHERE id_odber = id_odber;
+        WHERE id_odber_cursor = id_odber;
         mnozstvi_krve_odbery_celkem := mnozstvi_krve_odbery_celkem + mnozstvi_krve_odber;
     END LOOP;
     CLOSE odbery_cursor;
+END;
+/
+
+-- procedura pro pridani odberu do objednavky (BEZ KURZORU)
+-- prida odbery nachazejici se v zarizeni dodavatele objednavky, dokud neni dosazene mnozstvi krve polozky objednavky
+CREATE OR REPLACE PROCEDURE Pridani_odberu_do_objednavky_bez_kurzoru(
+    id_polozka IN Polozka_objednavky.id_polozka%TYPE
+) AS
+    id_objednavka_polozky Polozka_objednavky.fk_id_objednavka%TYPE;
+    mnozstvi_krve_polozka INTEGER;
+    id_odber_cursor Odber.id_odber%TYPE;
+    mnozstvi_krve_odber INTEGER;
+    mnozstvi_krve_odbery_celkem INTEGER;
+BEGIN
+    SELECT fk_id_objednavka INTO id_objednavka_polozky
+        FROM Polozka_objednavky
+        WHERE id_polozka = Pridani_odberu_do_objednavky_bez_kurzoru.id_polozka;
+
+    SELECT mnozstvi INTO mnozstvi_krve_polozka
+        FROM Polozka_objednavky
+        WHERE id_polozka = Pridani_odberu_do_objednavky_bez_kurzoru.id_polozka;
+        
+    mnozstvi_krve_odbery_celkem := 0;
+    LOOP
+        id_odber_cursor := NULL;
+        SELECT id_odber INTO id_odber_cursor
+            FROM Odber
+            WHERE fk_id_objednavka IS NULL AND
+            fk_id_zarizeni = (
+                SELECT fk_id_zarizeni_dodavatel
+                FROM Objednavka
+                WHERE id_objednavka = id_objednavka_polozky
+            ) AND
+            typ_krve = (
+                SELECT typ_krve
+                FROM Polozka_objednavky
+                WHERE id_polozka = Pridani_odberu_do_objednavky_bez_kurzoru.id_polozka
+            )
+            AND ROWNUM = 1;
+
+        EXIT WHEN id_odber_cursor IS NULL;
+
+        SELECT mnozstvi INTO mnozstvi_krve_odber
+            FROM Odber
+            WHERE id_odber = id_odber_cursor;
+        
+        UPDATE Odber SET fk_id_objednavka = id_objednavka_polozky
+        WHERE id_odber = id_odber_cursor;
+
+        mnozstvi_krve_odbery_celkem := mnozstvi_krve_odbery_celkem + mnozstvi_krve_odber;
+        EXIT WHEN mnozstvi_krve_odbery_celkem >= mnozstvi_krve_polozka;
+    END LOOP;
 END;
 /
 
@@ -246,6 +317,10 @@ INSERT INTO Osoba
 VALUES(osoba_seq.NEXTVAL,'440726/0673','Peter','Novotny',TO_DATE('26.07.1978', 'dd.mm.yyyy'),'123456789','novota@gmail.com', 3);
 INSERT INTO Osoba
 VALUES(osoba_seq.NEXTVAL,'440726/0674','Jan','Slovak',TO_DATE('26.07.1944', 'dd.mm.yyyy'),'123456789','slovensko123@gmail.com', 1);
+INSERT INTO Osoba
+VALUES(osoba_seq.NEXTVAL,'440726/0675','Petr','Cech',TO_DATE('26.07.1999', 'dd.mm.yyyy'),'123456789','cz123@gmail.com', 2);
+INSERT INTO Osoba
+VALUES(osoba_seq.NEXTVAL,'440726/0676','Peter','Slovak',TO_DATE('26.07.1978', 'dd.mm.yyyy'),'123456789','pptr@gmail.com', 3);
 
 -- DARCE
 INSERT INTO Darce
@@ -254,6 +329,13 @@ INSERT INTO Darce
 VALUES(2,'AB-',TO_DATE('12.10.2023', 'dd.mm.yyyy'));
 INSERT INTO Darce
 VALUES(3,'B+',TO_DATE('21.10.2023', 'dd.mm.yyyy'));
+INSERT INTO Darce
+VALUES(4,'A-',TO_DATE('10.10.2023', 'dd.mm.yyyy'));
+INSERT INTO Darce
+VALUES(5,'AB+',TO_DATE('12.10.2023', 'dd.mm.yyyy'));
+INSERT INTO Darce
+VALUES(6,'B-',TO_DATE('21.10.2023', 'dd.mm.yyyy'));
+
 
 -- ZAMESTNANEC
 INSERT INTO Zamestnanec
@@ -278,12 +360,64 @@ INSERT INTO Odber
 VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),1,1,NULL,'A+', 500);
 INSERT INTO Odber
 VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,1,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),4,2,NULL,'A-', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('12.10.2023', 'dd.mm.yyyy'),2,2,NULL,'AB-', 1000);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),4,2,NULL,'A-', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,2,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),1,2,NULL,'A+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('12.10.2023', 'dd.mm.yyyy'),2,2,NULL,'AB-', 1000);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),1,2,NULL,'A+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,2,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),4,2,NULL,'A-', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('12.10.2023', 'dd.mm.yyyy'),4,2,NULL,'A-', 1000);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),5,2,NULL,'AB+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,2,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),1,2,NULL,'A+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('12.10.2023', 'dd.mm.yyyy'),5,2,NULL,'AB+', 1000);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),1,2,NULL,'A+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,2,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),6,2,NULL,'B-', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('12.10.2023', 'dd.mm.yyyy'),6,2,NULL,'B-', 1000);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.4.2023', 'dd.mm.yyyy'),6,2,NULL,'B-', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('21.10.2023', 'dd.mm.yyyy'),3,2,NULL,'B+', 500);
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),4,2,NULL,'A-', 500);
 
 -- POLOZKA_OBJEDNAVKA
 INSERT INTO Polozka_objednavky
 VALUES(polozka_seq.NEXTVAL,1,1,'A+',1000);
 INSERT INTO Polozka_objednavky
-VALUES(polozka_seq.NEXTVAL,2,2,'AB-',500);
+VALUES(polozka_seq.NEXTVAL,1,2,'AB-',500);
+INSERT INTO Polozka_objednavky
+VALUES(polozka_seq.NEXTVAL,1,1,'A+',500);
+INSERT INTO Polozka_objednavky
+VALUES(polozka_seq.NEXTVAL,1,1,'B+',900);
+INSERT INTO Polozka_objednavky
+VALUES(polozka_seq.NEXTVAL,1,2,'A+',600);
+INSERT INTO Polozka_objednavky
+VALUES(polozka_seq.NEXTVAL,1,2,'AB+',500);
+INSERT INTO Polozka_objednavky
+VALUES(polozka_seq.NEXTVAL,1,2,'B-',400);
 
 -- ODBER_ZAMESTNANEC
 INSERT INTO Odber_Zamestnanec
@@ -295,6 +429,7 @@ VALUES(odber_zamestnanec_seq.NEXTVAL, 2,3);
 
 COMMIT;
 
+-- Prideleni prav druhemu clenovi tymu
 GRANT ALL ON Osoba TO XKEJDO00;
 GRANT ALL ON Darce TO XKEJDO00;
 GRANT ALL ON Zamestnanec TO XKEJDO00;
@@ -336,9 +471,31 @@ SELECT
 FROM MnozstviObjednaneKrve t
 RIGHT JOIN Zarizeni z ON t.fk_id_zarizeni_objednavatel = z.id_zarizeni;
 
--- Tato cast scriptu by se mela spustit druhym clenem tymu
+-- Predvedeni triggeru
+-- Vytvoreni nove objednavky
+INSERT INTO Objednavka (id_objednavka, fk_id_zarizeni_objednavatel, fk_id_zarizeni_dodavatel)
+VALUES(objednavka_seq.NEXTVAL, 1, 2);
+SELECT * FROM Objednavka WHERE id_objednavka = 3;
+-- Vytvoreni spatne objednavky - zarizeni objednavatel a dodavatel je stejne
+INSERT INTO Objednavka (id_objednavka, fk_id_zarizeni_objednavatel, fk_id_zarizeni_dodavatel)
+VALUES(objednavka_seq.NEXTVAL, 1, 1);
+-- Vytvoreni spatneho odberu - typ krve neodpovida typu krve darce
+INSERT INTO Odber
+VALUES(odber_seq.NEXTVAL,TO_DATE('10.10.2023', 'dd.mm.yyyy'),1,1,NULL,'B+', 500);
+
+-- Predvedeni procedur
+-- Pridani odberu do objednavky s kurzorem
+EXEC Pridani_odberu_do_objednavky(1);
+SELECT * FROM Odber WHERE fk_id_objednavka = 1;
+EXEC Pridani_odberu_do_objednavky(2);
+SELECT * FROM Odber WHERE fk_id_objednavka = 2;
+-- Dokoceni objednavky
+EXEC Dokonceni_objednavky(1);
+SELECT * FROM Objednavka WHERE id_objednavka = 1;
+SELECT * FROM Odber WHERE id_odber = 1 OR id_odber = 3;
+
+-- VIEW Seznam všech dárců a počet jejich darování, tato cast scriptu by se mela spustit druhym clenem tymu
 DROP MATERIALIZED VIEW Darci_a_pocet_odberu;
--- VIEW Seznam všech dárců a počet jejich darování
 CREATE MATERIALIZED VIEW Darci_a_pocet_odberu
 REFRESH ON DEMAND AS
     SELECT b.jmeno, b.prijmeni, COUNT(o.id_odber) AS odberu_celkem
@@ -346,3 +503,25 @@ REFRESH ON DEMAND AS
     LEFT JOIN XKATON00.Odber o ON d.id_darce_fk_osoba = o.fk_id_darce
     JOIN XKATON00.Osoba b ON d.id_darce_fk_osoba = b.id_osoba
     GROUP BY b.jmeno, b.prijmeni ORDER BY COUNT(o.id_odber) DESC;
+COMMIT;
+SELECT * FROM Darci_a_pocet_odberu;
+
+-- EXPLAIN PLAN pro puvodni dotaz a dotaz s indexem
+explain plan for
+SELECT b.jmeno, b.prijmeni, COUNT(o.id_odber) AS odberu_celkem
+FROM Odber o
+JOIN Osoba b ON o.fk_id_darce = b.id_osoba
+GROUP BY b.jmeno, b.prijmeni ORDER BY COUNT(o.id_odber) DESC;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+-- Index na fk_id_darce, ktery se pouziva v dotazu pro zjisteni poctu odberu jednotlivych darcu
+-- diki indexu se snizi cas vyhledavani a zpracovani dotazu na tabulku Odber
+CREATE INDEX idx_fk_id_darce ON Odber(fk_id_darce);
+COMMIT;
+explain plan for
+SELECT b.jmeno, b.prijmeni, COUNT(o.id_odber) AS odberu_celkem
+FROM Odber o
+JOIN Osoba b ON o.fk_id_darce = b.id_osoba
+GROUP BY b.jmeno, b.prijmeni ORDER BY COUNT(o.id_odber) DESC;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+DROP INDEX idx_fk_id_darce;
+COMMIT;
